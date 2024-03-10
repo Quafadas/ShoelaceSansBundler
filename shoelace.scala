a5670e2032a25327368d5a8370c80cb24859ffb4
Simon Parten <quafadas@gmail.com>
2024-03-10 09:01:48 +0100

    .

--- shoelace.scala
+++ shoelace.scala
@@ -4,8 +4,8 @@
 //> using dep com.raquo::laminar-shoelace::0.1.0-M2
 //> using dep com.raquo::laminar::17.0.0-M6
 
-//> using dep io.github.quafadas::dedav4s::0.9-27f83db-20240308T172356Z-SNAPSHOT
-//> using dep io.github.quafadas::dedav_laminar::0.9-27f83db-20240308T172356Z-SNAPSHOT
+//> using dep io.github.quafadas::dedav4s::0.9.0-RC11
+//> using dep io.github.quafadas::dedav_laminar::0.9.0-RC11
 
 //> using jsModuleKind es
 //> using jsEsModuleImportMap importmap.json
